8679a167254b0d34ee50c7efb9b94a1124af1ac3
Bhargav Bachina <bhargavbachina@Bhargavs-MacBook-Pro.local>
2021-12-06 21:25:24 -0600

    changes

---
+++ ui/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header">
+        Vue.js API Parallel Calls
+    </div>
+</template>
+
+<script>
+    console.log("I am in header!!!")
+    export default {
+        name: 'Header'
+    }
+</script>
